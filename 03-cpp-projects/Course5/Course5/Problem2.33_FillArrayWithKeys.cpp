bd6a7023dbbd914b6ab3b24cab990be2066a6adf
HamzaSahaban-AIEngineering-Student <h.shaban2007@hotmail.com>
2026-08-04 12:34:31 +0300

    لاالابللا

---
+++ 03-cpp-projects/Course5/Course5/Problem2.33_FillArrayWithKeys.cpp
@@ -0,0 +1,17 @@
+/*
+Problem:
+Write a program to read how many keys to generate and fill
+them in array then print them on the screen.
+
+Input:
+5
+
+Output:
+Array elements:
+
+Array[0] : NRXC-DXRO-QEAM-WRWE
+Array[1] : BJTR-KIKF-ATEW-WNTT
+Array[2] : QOFX-KZEL-SSDP-AKSI
+Array[3] : GFOI-ELFX-ZCVE-PSTP
+Array[4] : MATF-PRQA-PQEQ-TRIM
+*/
